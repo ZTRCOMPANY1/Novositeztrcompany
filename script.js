2c97a66b230eb7e8c8e17baa5493d7b3aecc4301
ZTRCOMPANY1 <ztrcompany1@gmail.com>
2025-03-18 00:17:11 -0300

    2%

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function baixarArquivo() {
-    alert("Iniciando download...");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
