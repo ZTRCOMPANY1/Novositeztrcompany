fc78e5d19392f830a20076cc3f1b9507cb25e397
ZTRCOMPANY1 <ztrcompany1@gmail.com>
2025-03-24 17:43:44 -0300

    retirado um sistema

--- script.js
+++ script.js
@@ -15,17 +15,3 @@ document.addEventListener('keydown', function(e) {
   });
 
 
-// Exemplo básico de navegação sem recarregar a página
-document.querySelectorAll('a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const page = this.getAttribute('href').substring(1);
-      loadPage(page);  // Função que carrega a página via JavaScript
-      window.history.pushState({ page }, '', this.getAttribute('href'));
-    });
-  });
-  
-  function loadPage(page) {
-    // Função para carregar o conteúdo da página dinamicamente
-    document.querySelector('#content').innerHTML = `<h1>Conteúdo da página ${page}</h1>`;
-  }
